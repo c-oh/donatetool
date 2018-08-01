040573023c4846143a9e4e6bf63c1f3054b1bb92
Holly Cheng <papiniu@outlook.com>
2018-07-31 22:46:55 -0700

    working donate tool

--- donateScripts.js
+++ donateScripts.js
@@ -13,15 +13,32 @@ var ask = getParameterByName('ask');
 if (ask <= 0 || ask >= 1200) {
     ask = 5
 }
+
 document.getElementById("donation_amount_1").value = ask;
 document.getElementById("label1").innerText = ask;
+var legalMax = 1200;
 
-var askInc1 = ask*2;
-var askInc2 = ask*3;
-var askInc3 =ask*4;
-var askInc4 =ask*5;
-var askInc5 =ask*6;
-var askInc6 =ask*7;
+function round5(x)
+{
+    return Math.ceil(x/5)*5;
+}
+
+if(ask < 40){
+    var askInc1 = round5(ask*2);
+    var askInc2 = round5(ask*3);
+    var askInc3 =round5(ask*4);
+    var askInc4 =round5(ask*5);
+    var askInc5 =round5(ask*6);
+    var askInc6 =round5(ask*7);
+} else if(ask > 40){
+    var askInc1 = round5(ask*1.5);
+    var askInc2 = round5(ask*2);
+    var askInc3 =round5(ask*2.5);
+    var askInc4 =round5(ask*3);
+    var askInc5 =round5(ask*3.5);
+    var askInc6 = round5(ask*4);
+} 
+
 
 document.getElementById("donation_amount_2").value = askInc1;
 document.getElementById("label2").innerText = askInc1;
